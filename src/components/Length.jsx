ac75d7db9618dc2a86cf0d775bbfd6518ccd5e7b
mkielak <kielakowsky17@gmail.com>
2025-10-27 15:42:35 +0100

    Added decrementation and incrementation for Length component

--- src/components/Length.jsx
+++ src/components/Length.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
-const Length = ({children, value}) => {
-  return (
-    <div>
-        <h2>{children}</h2>
-        <p><span><button>-</button>{value}<button>+</button></span></p>
-    </div>
-  )
+const Length = ({children, value, onUpdate}) => {
+
+    const handleDecrement = () => {
+        if (value > 1) {
+            onUpdate((prevValue) => prevValue - 1);
+        }
+    }
+
+    const handleIncrement = () => {
+        if (value < 60) {
+            onUpdate( (prevValue) => prevValue + 1 );
+        }
+    }
+
+    return (
+        <div>
+            <h2>{children}</h2>
+            <p><span><button onClick={handleDecrement}>-</button>{value}<button onClick={handleIncrement}>+</button></span></p>
+        </div>
+    )
 }
 
 export default Length
